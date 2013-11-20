acfb4fb4a4220d08e291268ec9db20ff72d7e03d
sagimbekov <ilyas.sagimbekov@gmail.com>
2013-11-20 23:50:43 +0600

    added new files

---
+++ index.php
@@ -0,0 +1,252 @@
+
+<!doctype html>
+<!--[if IE 8]> <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 9]> <!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<!--[if lt IE 9]> <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+
+<head>
+<title>Rich Dean, creative designer specialising in digital media</title>
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<meta name="description" content="I'm Rich Dean, a creative designer specialising in digital media with over 12 years of experience. I work as Head of design for London agency Zone, where we produce digital goodness for some of the best brands in the world." />
+<meta name="viewport" content="width=device-width" />
+<meta name="apple-touch-fullscreen" content="YES" />
+<meta name="apple-mobile-web-app-capable" content="YES" />
+<link rel="shortcut icon" href="img/icons/favicon.ico" />
+<link rel="apple-touch-icon" href="img/icons/touch_icon.png" />
+<link rel="stylesheet" type="text/css" href="css/reset.css" />
+<link rel="stylesheet" type="text/css" href="css/global.css" />
+<script src="js/libs/modernizr-2.5.3.min.js"></script>
+
+
+</head>
+
+<body class="home">
+
+<!-- White overlay for elegant load -->
+<div id="white-overlay"> </div>
+
+<!-- Header -->
+<header class="outer-container expanded" id="header">
+    <div class="inner-container">
+        <section id="rd_logo"> <img src="img/ui/logo.gif"/> </section>
+        
+        <nav id="primary">
+            <label for="toggle-menu">Menu <img src="img/ui/menu.png" alt="Menu" width="80" height="80" /></label>
+            <a href="#intro" class="current">Home</a> <a href="#portfolio-header">Portfolio</a> <a href="#about">About</a> <a href="#twitter">Latest</a> <a href="#clients">Clients</a> </nav>
+    </div>
+</header>
+<section class="outer-container" id="intro">
+    <div class="inner-container">
+        <article>
+            <h1 class="hidden">RichDean.co.uk - personal portfolio site of digital designer Rich Dean.</h1>
+            <p>Hello</p>
+            <p>I'm Rich Dean, a creative designer specialising in digital media with over 12 years of experience. I work as Head of design for London agency <a href="http://www.thisiszone.com" target="_blank">Zone</a>, where we produce digital goodness for some of the best brands in the world.</p>
+            <a href="#about" class="button text-only scroll"> Find out more </a> </article>
+    </div>
+</section>
+
+<!-- Portfolio Header -->
+<section class="outer-container" id="portfolio-header">
+    <div class="inner-container">
+        <h2>Portfolio</h2>
+    </div>
+</section>
+
+<!-- Ajax Container -->
+<section class="outer-container" id="ajax-content">
+    <div class="inner-container ajax-container">
+        <article class="content"> </article>
+    </div>
+</section>
+
+<!-- Portfolio Container -->
+<section class="outer-container" id="portfolio">
+    <div class="inner-container">
+        <article class="invisible">
+            <div class="thumb"> <a href="npc.htm#portfolio-header" class="ajax"> <span class="thumb-border"></span> <img src="img/thumbs/thumb_npc.jpg" alt="NPC"class="thumb-img" /> </a> </div>
+            <div class="summary">
+                <h3> <a href="npc.htm#portfolio-header" class="ajax"> NPC </a> </h3>
+                <p> Website redesign </p>
+            </div>
+        </article>
+        <article class="invisible">
+            <div class="thumb"> <a href="lucerne.htm#portfolio-header" class="ajax"> <span class="thumb-border"></span> <img src="img/thumbs/thumb_lucerne.jpg" alt="Lucerne"class="thumb-img" /> </a> </div>
+            <div class="summary">
+                <h3> <a href="lucerne.htm#portfolio-header" class="ajax"> Lucerne  Festival </a> </h3>
+                <p> Website redesign (concept) </p>
+            </div>
+        </article>
+        <article class="invisible">
+            <div class="thumb"> <a href="cce.htm#portfolio-header" class="ajax"> <span class="thumb-border"></span> <img src="img/thumbs/thumb_cce.jpg" alt="Coca-Cola Enterprises"class="thumb-img" /> </a> </div>
+            <div class="summary">
+                <h3> <a href="cce.htm#portfolio-header" class="ajax"> Coca-Cola Enterprises </a> </h3>
+                <p> Website redesign (pitch) </p>
+            </div>
+        </article>
+        <article class="invisible">
+            <div class="thumb"> <a href="relentless.htm#portfolio-header" class="ajax"> <span class="thumb-border"></span> <img src="img/thumbs/thumb_relentless.jpg" alt="Relentless Energy Drink"class="thumb-img" /> </a> </div>
+            <div class="summary">
+                <h3> <a href="relentless.htm#portfolio-header" class="ajax"> Relentless Energy Drink </a> </h3>
+                <p> Digital communications </p>
+            </div>
+        </article>
+        <article class="invisible">
+            <div class="thumb"> <a href="relentless_qr.htm#portfolio-header" class="ajax"> <span class="thumb-border"></span> <img src="img/thumbs/thumb_relentlessqr.jpg" alt="Relentless Energy Drink"class="thumb-img" /> </a> </div>
+            <div class="summary">
+                <h3> <a href="relentless_qr.htm#portfolio-header" class="ajax"> Relentless Energy Drink </a> </h3>
+                <p> Freeze Festival QR campaign </p>
+            </div>
+        </article>
+        <article class="invisible">
+            <div class="thumb"> <a href="zone.htm#portfolio-header" class="ajax"> <span class="thumb-border"></span> <img src="img/thumbs/thumb_zone.jpg" alt="Zone"class="thumb-img" /> </a> </div>
+            <div class="summary">
+                <h3> <a href="zone.htm#portfolio-header" class="ajax"> Zone </a> </h3>
+                <p> Agency website </p>
+            </div>
+        </article>
+        <article class="invisible">
+            <div class="thumb"> <a href="pepperhood.htm#portfolio-header" class="ajax"> <span class="thumb-border"></span> <img src="img/thumbs/thumb_drpepper.jpg" alt="Dr Pepper"class="thumb-img" /> </a> </div>
+            <div class="summary">
+                <h3> <a href="pepperhood.htm#portfolio-header" class="ajax"> Dr Pepper </a> </h3>
+                <p> The Pepperhood </p>
+            </div>
+        </article>
+       
+    </div>
+</section>
+
+<!-- About Container -->
+<section class="outer-container" id="about">
+    <div class="inner-container">
+        <h2>About</h2>
+        <p class="statement">I make things on the internet</p>
+        <div class="info">
+            <p>I'v been working as a designer for the past thirteen years, and in that time I've worked on some amazing projects with some really talented people. I've learned a lot along the way. Three years ago I joined Zone, a top digital agency based in London, and for the past two years I've held the role of head of design. My job is evenly split between hands-on design and management duties, and it's great to be able to pass on my skills and help shape the way the agency works.</p>
+            <div class="thumb invisible"> <img src="img/thumbs/thumb_drpepper.jpg" alt="Myself working at a desk" width="640" height="640" class="thumb-img" /> </div>
+            <p>When I'm not designing, I like to keep fit and healthy, either running or working out at the gym. I'm also a big fan of sci-fi and action films, and when I get the time, I like computer games too. I enjoy a spot of DIY, but I'm also quite and outdoors kind of person. Family and friends are probably most important to me, and the best times are always holidays away or quality nights enjoying a drink with friends.</p>
+        </div>
+        <article class="pie">
+            <div class="pie_chart">
+                <div class="outer invisible" id="user_experience" ></div>
+                <div class="inner invisible"> <img src="img/skills/user_experience.png" alt="Illustration of light bulb" width="100" height="100" /> </div>
+            </div>
+            <div class="summary">
+                <h3>User experience</h3>
+                <p>I'm an advocate of user-centred design, and I believe that simple and intuitive user interaction is just as important as visual layout. Before starting full designs, I use Axure to build sitemaps, wireframes and interactive prototypes.</p>
+            </div>
+        </article>
+        <article class="pie">
+            <div class="pie_chart">
+                <div class="outer invisible" id="visual_design" ></div>
+                <div class="inner invisible"> <img src="img/skills/visual.png" alt="Illustration of paintbrush" width="100" height="100" /> </div>
+            </div>
+            <div class="summary">
+                <h3>Visual design</h3>
+                <p>Once the user interaction and wireframes have been created, the visual design stage is where it starts to come to life. This is by far my speciality, and my prefered tool is Photoshop for perfecting the visual style of a project.</p>
+            </div>
+        </article>
+        <article class="pie">
+            <div class="pie_chart">
+                <div class="outer invisible" id="development" ></div>
+                <div class="inner invisible"> <img src="img/skills/development.png" alt="Illustration of cogs" width="100" height="100" /> </div>
+            </div>
+            <div class="summary">
+                <h3>Development</h3>
+                <p>While I'm primarily a designer, the inner workings of a website fundamentally affect how we design, and so I beleive its important to keep up to date with technology. I have good experience of hand-coding HTML, CSS and JavaScript.</p>
+            </div>
+        </article>
+        <article class="pie">
+            <div class="pie_chart">
+                <div class="outer invisible" id="responsive_design" ></div>
+                <div class="inner invisible"> <img src="img/skills/responsive.png" alt="Illustration of multiple screens" width="100" height="100" /> </div>
+            </div>
+            <div class="summary">
+                <h3>Responsive design</h3>
+                <p>The way we interact with and consume content on the internet is constantly evolving, and so for the past two years I have been working on a content-led approach that uses a 1000px grid system to design sites that are fully responsive.</p>
+            </div>
+        </article>
+    </div>
+</section>
+
+<!-- Section containing Twitter feed -->
+<section class="outer-container" id="twitter">
+    <div class="inner-container">
+        <h2>Latest from Twitter</h2>
+        <article>
+            <p><a href="http://www.twitter.com/#!/richdean123" target="_blank">@richdean123</a></p>
+            <div id="tweet-fill">
+                <p>Very proud of the new <a href="https://twitter.com/#!/thisiszone" target="_blank">@thisiszone</a> website: <a href="http://www.thisiszone.com" target="_blank">http://www.thisiszone.com</a></p>
+                <p>1 day ago</p>
+            </div>
+            <div id="twitter_update_list"></div>
+            <a href="http://twitter.com/#!/richdean123" class="button with-image" target="_blank">Follow me<img src="img/icons/icon_twitter.png" alt="Twitter" width="80" height="80" /></a> </article>
+    </div>
+</section>
+
+<!-- Section containing Instagram feed -->
+<section class="outer-container" id="instagram-feed">
+    <div class="inner-container">
+        <h2>Latest from Instagram</h2>
+        <div id="instagram-fill">
+            <article class="invisible">
+                <div class="thumb"> <img src="img/instagram/IMG_1302.jpg" alt="Zone"class="thumb-img" /> </div>
+            </article>
+            <article class="invisible">
+                <div class="thumb"> <img src="img/instagram/IMG_1349.jpg" alt="Zone"class="thumb-img" /> </div>
+            </article>
+            <article class="invisible">
+                <div class="thumb"> <img src="img/instagram/IMG_1354.jpg" alt="Zone"class="thumb-img" /> </div>
+            </article>
+            <article class="invisible">
+                <div class="thumb"> <img src="img/instagram/IMG_1357.jpg" alt="Zone"class="thumb-img" /> </div>
+            </article>
+        </div>
+        <div class="instagram invisible"></div>
+        <div class="six-column-center"> <a href="http://instagram.com/richdean123/" class="button with-image" target="_blank">See my feed<img src="img/icons/icon_instagram.png" alt="Instagram" width="80" height="80" /></a> </div>
+    </div>
+</section>
+
+<!-- Clients -->
+<section class="outer-container" id="clients">
+    <div class="inner-container">
+        <h2>Clients</h2>
+        <!-- <article class="invisible">
+            <div class="thumb"> <img src="img/logos/logo_adidas.png" alt="Adidas" width="200" height="200" class="thumb-img" /> </div>
+        </article>
+        <article class="invisible">
+            <div class="thumb"> <img src="img/logos/logo_bt.png" alt="BT" width="200" height="200" class="thumb-img" /> </div>
+        </article>
+        <article class="invisible">
+            <div class="thumb"> <img src="img/logos/logo_cocacola.png" alt="Coca-Cola" width="200" height="200" class="thumb-img" /> </div>
+        </article>
+        <article class="invisible">
+            <div class="thumb"> <img src="img/logos/logo_drpepper.png" alt="Dr Pepper" width="200" height="200" class="thumb-img" /> </div>
+        </article>
+        <article class="invisible">
+            <div class="thumb"> <img src="img/logos/logo_ea.png" alt="EA Games" width="200" height="200" class="thumb-img" /> </div>
+        </article> -->
+ 
+    </div>
+</section>
+
+<!-- Clients -->
+<footer class="outer-container">
+    <div class="inner-container">
+        <p id="copyright">This site design and build © Rich Dean 2013</p>
+        <nav id="contacts"> <a href="http://www.twitter.com/richdean123" class="twitter" target="_blank"> <img src="img/icons/icon_twitter.png" alt="Twitter" width="80" height="80" /> </a> <a href="http://instagram.com/richdean123/" class="insta-gram" target="_blank"> <img src="img/icons/icon_instagram.png" alt="Instagram" width="80" height="80" /> </a> <a href="http://www.linkedin.com/profile/view?id=15642744&locale=en_US&trk=tab_pro" class="linkedin" target="_blank"> <img src="img/icons/icon_linkedin.png" alt="LinkedIn" width="80" height="80" /> </a> <a href="mailto:rich@richdean.co.uk" class="email"> <img src="img/icons/icon_email.png" alt="Email" width="80" height="80" /> </a> </nav>
+    </div>
+</footer>
+</div>
+<script src="http://ajax.microsoft.com/ajax/jquery/jquery-1.7.2.min.js"></script> 
+<script>window.jQuery || document.write('<script src="js/libs/jquery-1.7.2.min.js"><\/script>')</script> 
+<script type="text/javascript" src="js/plugins.min.js"></script> 
+<script type="text/javascript" src="js/script.min.js"></script> 
+<script>
+    var _gaq=[['_setAccount','UA-32939331-1'],['_trackPageview']];
+    (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g,s)}(document,'script'));
+</script>
+</body>
+</html>
